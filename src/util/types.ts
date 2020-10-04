0db4e5b0ca3e90bcb8cc661a4eb9479e5b4b673d
James Nugent <james.nugent89@gmail.com>
2020-10-04 21:54:30 +1100

    Added types to utils

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,1 +1,8 @@
 export const NOOP = () => {};
+
+export type Runnable = () => void;
+export type Supplier<T> = () => T;
+export type Consumer<T> = (a: T) => void;
+export type BiConsumer<T, S> = (a: T, b: S) => void;
+export type Function<T, S> = (a: T) => S;
+export type BiFunction<T, S, U> = (a: T, b: S) => U;
